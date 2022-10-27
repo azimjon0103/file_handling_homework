f54ae7623bbb24444e5241ba65da1fb098967b67
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-27 11:33:17 +0500

    6

--- file06.py
+++ file06.py
@@ -7,4 +7,13 @@ def main(data:str):
         list: return answer
     """
     
+    ls=data.split('\n')
+    ans=[]
+    for i in ls:
+        ans.append(len(str(i)))
+    return ans
 # Read data from file
+f=open("./txt_file/data06.txt")
+data=f.read()
+print(main(data))
+    
